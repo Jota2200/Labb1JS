4bf2c61228d185bfc95d4c8252f63b9e21edb1a8
Josef Tamsamani <jota2200@student.miun.se>
2022-11-14 15:43:00 +0100

    Labb2

---
+++ Labb2/js/app.js
@@ -0,0 +1,31 @@
+const news = [
+  {
+    headline: "Regeringen skjuter på publiklättnader",
+    author: "Erik Wikén",
+    href: "https://www.svt.se/nyheter/inrikes/regeringen-skjuter-pa-publiklattnader",
+    summary:
+      "Det blir inga lättnader i publikrestriktioner som planerat – åtminstone inte nu. Det beskedet gav regeringen i dag. Orsaken är den återigen ökade smittspridningen i samhället.",
+  },
+  {
+    headline: "Graffitin – så kom den till Sverige genom Public service",
+    author: "",
+    href: "https://www.svt.se/nyheter/lokalt/ost/graffitin-sa-kom-den-till-sverige-genom-public-service",
+    summary:
+      "Det går faktiskt nästan att fastställa på minuten när graffitin kom till Sverige. Se hur det gick till och hur subkulturen har utvecklats sedan dess i videon.",
+  },
+  {
+    headline: "Louise Glück får Nobelpriset i litteratur",
+    author: ["Jakob Runevad Kjellmer", "Lydia Farran-Lee", "Sebastian Folcker"],
+    href: "https://www.svt.se/kultur/nobelpriset-i-litteratur-2020",
+    summary:
+      "Den amerikanska poeten Louise Glück mottar Nobelpriset i litteratur 2020. Hon debuterade med Firstborn 1968 och anses vara en av de mest tongivande samtida amerikanska poeterna.",
+  },
+];
+
+/**
+ *
+ * Här ska du skriva din javascript för att ändra innehållet på webbsidan.
+ *
+ */
+
+console.log("js loaded!");
